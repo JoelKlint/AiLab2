abe01b1bee943ef77a28409078dc4bb6d02ceec7
Oscar Rydh <oscar.rydh.93@gmail.com>
2017-02-15 15:29:32 +0100

    Adding the latest stuff

---
+++ LibvismReader.py
@@ -0,0 +1,31 @@
+
+
+def readFile(file, langValue):
+
+    out = ''
+    for line in file:
+        info = line.split('\t')
+        out += ('%s 1:%s 2:%s \n' %(langValue, info[0].strip(), info[1].strip()))
+
+    return out
+
+english = open('english.txt', 'r')
+eng_res = readFile(english, '+1')
+english.close()
+
+writer = open('english_formated.txt', 'w')
+writer.write(eng_res)
+writer.close()
+
+french = open('french.txt', 'r')
+fr_res = readFile(french, '0')
+french.close()
+
+writer = open('french_formated.txt', 'w')
+writer.write(fr_res)
+writer.close()
+
+writer = open('total_formated.txt', 'w')
+writer.write(eng_res)
+writer.write(fr_res)
+writer.close()
